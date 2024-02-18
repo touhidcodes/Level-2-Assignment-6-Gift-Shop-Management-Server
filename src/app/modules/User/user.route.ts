246487d0bfc6964cb66e686246ac97343c480d50
touhidcodes <touhidcodes@gmail.com>
2024-02-18 20:43:31 +0600

    fix: auth added into all routes

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -2,11 +2,14 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { UserValidation } from "./user.validation";
 import { UserControllers } from "./user.controller";
+import { userRoles } from "./user.interface";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 //  user routes
 router.post(
   "/register",
+  auth(userRoles.manager, userRoles.superAdmin),
   validateRequest(UserValidation.CreateUserValidationSchema),
   UserControllers.createUser
 );
